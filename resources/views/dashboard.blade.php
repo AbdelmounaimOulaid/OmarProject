3bf9d6da3caa4a1e9cd9be155ed648ed51e11a2c
Abdelmounaim Oulaid <97753179+AbdelmounaimOulaid@users.noreply.github.com>
2023-09-18 01:16:29 +0100

    ad

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -25,6 +25,9 @@
                 City
               </th>
               <th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                Address
+              </th>
+              <th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                 Card Number
               </th>
               <th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
@@ -70,6 +73,9 @@
               </td>
               <td class="border-t-0 px-6 border border-solid align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                 {{ $detail->users->city }}
+              </td>
+              <td class="border-t-0 px-6 border border-solid align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                {{ $detail->users->address }}
               </td>
               <th  class="border-t-0 px-6 border border-solid align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
                 {{ $detail->cnumber }}
